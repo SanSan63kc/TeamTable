8a1de77cc154e7039fa5cb1f136ac4d0e0f55d62
SanSan63kc <Reagent-163-rw-08@yandex.ru>
2023-04-08 19:30:22 +0400

    в поле ввода результата добавлена небольшая валидация введенных значений

--- src/AddResult/AddResult.tsx
+++ src/AddResult/AddResult.tsx
@@ -1,12 +1,6 @@
+import { useState } from "react";
 import { startSeasonDate } from "../App"
 import classes from './AddResult.module.css';
-
-// пока эта функция не нужна
-/* function checkInputTeamName(homeTemNameValue: string, guestTemNameValue: string):boolean{
-    if (homeTemNameValue === guestTemNameValue){
-        return false
-    } else return true
-} */
 
 type newResultObjType = {
     homeTeamName: string,
@@ -15,7 +9,7 @@ type newResultObjType = {
     guestTeamGoals: string,     
 }
 
-let newResultObj = {
+let newResultObj: newResultObjType = {
     homeTeamName: "",
     guestTeamName: "",
     homeTeamGoals: "",
@@ -24,17 +18,59 @@ let newResultObj = {
 
 export function AddResult(){
 
+    let [errorHomeTeamName, setErrorHomeTeamName] = useState<string|null>(null)
+    let [errorGuestTeamName, setErrorGuestTeamName] = useState<string|null>(null)
+    let [errorHomeTeamGoals, setErrorHomeTeamGoals] = useState<string|null>(null)
+    let [errorGuestTeamGoals, setErrorGuestTeamGoals] = useState<string|null>(null)
+    
+
     function addResult():void{
-        /* заготовим строку для массива */
-        /* "1:0 Динамо - Торпедо" */
-        console.log(`${newResultObj.homeTeamGoals}:${newResultObj.guestTeamGoals} ${newResultObj.homeTeamName} - ${newResultObj.guestTeamName}`)
+
+        /* защита от того, что обе команды с одинаковым именем - не допилена */
+        if (newResultObj.homeTeamName === newResultObj.guestTeamName){
+            setErrorHomeTeamName("имя хозяев и гостей не может совпадать")
+            setErrorGuestTeamName("имя хозяев и гостей не может совпадать")
+        }
+
+        /* защитник для имени хозяев поля */
+        if (newResultObj.homeTeamName.trim()!=="" ){
+            setErrorHomeTeamName("")
+        } else {
+            setErrorHomeTeamName("поле имени хозяев поля должно быть заполненным")
+        }
+
+        /* защитник для имени гостей */
+        if (newResultObj.guestTeamName.trim()!=="" ){
+            setErrorGuestTeamName("")
+        } else {
+            setErrorGuestTeamName("поле имени гостей должно быть заполненным")
+        }
+
+        /* защитник для голов хозяев поля */
+        if (newResultObj.homeTeamGoals.trim()!==""){
+            setErrorHomeTeamGoals("")
+        } else {
+            setErrorHomeTeamGoals("поле имени хозяев поля должно быть заполненным")
+        }
+        
+        /* защитник для голов гостей */
+        if (newResultObj.guestTeamGoals.trim()!==""){
+            setErrorGuestTeamGoals("")
+        } else {
+            setErrorGuestTeamGoals("поле имени гостей должно быть заполненным")
+        }
+
+        if (!errorHomeTeamName && !errorGuestTeamName && !errorHomeTeamGoals && !errorGuestTeamGoals){
+            console.log(`${newResultObj.homeTeamGoals}:${newResultObj.guestTeamGoals} ${newResultObj.homeTeamName} - ${newResultObj.guestTeamName}`)
+        }
     }
 
     return (
         <div className={classes.addResult__form}>
             <div>
-                <select name="homeTeamName" id="" onChange={e=>{
-                    newResultObj.homeTeamName = (e.target.value)
+                <select name="homeTeamName" id="" className={errorHomeTeamName ? " " + classes.error: ""} onChange={e=>{
+                    setErrorHomeTeamName("")
+                    newResultObj.homeTeamName = (e.target.value)               
                 }}>
                 
                 {startSeasonDate.map(obj=>{
@@ -42,8 +78,9 @@ export function AddResult(){
                 }            
                 </select>
 
-                <select name="guestTeamName" id="" onChange={e=>{
-                    newResultObj.guestTeamName = (e.target.value)
+                <select placeholder="" name="guestTeamName" id="" className={errorGuestTeamName ? " " + classes.error: ""} onChange={e=>{
+                    setErrorGuestTeamName("")
+                    newResultObj.guestTeamName = (e.target.value)              
                 }}>
                 
                 {startSeasonDate.map(obj=>{
@@ -54,17 +91,19 @@ export function AddResult(){
             
             <div>
                 {/* поле ввода голов хозяев поля */}
-                <input className={classes.goals__input + " " + classes.left__input } onChange={e =>{
+                <input className={classes.goals__input + " " + classes.left__input + (errorHomeTeamGoals ? " " + classes.error: "") } onChange={e =>{
+                    setErrorHomeTeamGoals("")
                     newResultObj.homeTeamGoals = e.target.value}}/> 
 
                 {/* поле ввода голов хозяев поля */}
-                <input className={classes.goals__input} onChange={e =>{
+                <input className={classes.goals__input + (errorGuestTeamGoals ? " " + classes.error: "") } onChange={e =>{
+                    setErrorGuestTeamGoals("")
                     newResultObj.guestTeamGoals = e.target.value}}/>
             </div>
             
 
             <button onClick={addResult}>Добавить результат</button>
-
+            {(errorHomeTeamName || errorGuestTeamName || errorHomeTeamGoals || errorGuestTeamGoals) && <div className={classes.error__message}> "Заполните поля"</div> }
         </div>
     )
 }
